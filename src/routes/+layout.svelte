8cea832a4aaa7ca35134095f115627834513c1c7
Michal Dyczkowski <themich4@gmail.com>
2024-02-03 17:29:26 +0100

    Add logo and TMDB attribution

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,39 @@
 <script>
-	import '../app.css';
+	import '../styles.css';
+	import logo from '$lib/images/logo.svg';
+	import tmdb from '$lib/images/tmdb.svg';
 </script>
 
-<nav>
-	<a href="/">Svelteflix</a>
+<nav class="flex w-full h-12 items-center justify-between">
+	<a href="/"><img alt="Svelteflix" src={logo} class="h-4" /></a>
+
+	<div class="flex gap-4">
+		<a href="/search">Search</a>
+		<a href="/watchlist">Watchlist</a>
+		<a href="/login">Login</a>
+	</div>
 </nav>
+
+<main>
+	<slot />
+</main>
+
+<footer class="flex justify-center h-16">
+	<p>
+		Data provided by
+		<a href="https://www.themoviedb.org"><img alt="The Movie DB" src={tmdb} /></a>
+	</p>
+</footer>
+
+<style>
+	nav {
+		max-width: var(--column);
+		padding: 0 var(--side);
+		color: var(--accent);
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+</style>
