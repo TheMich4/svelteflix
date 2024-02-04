f151092522c0b2f68b441e20bf29a76c5d0e67c9
Michal Dyczkowski <themich4@gmail.com>
2024-02-04 12:33:08 +0100

    Add movie details and trailer to movie page

--- src/routes/movies/[id]/+page.svelte
+++ src/routes/movies/[id]/+page.svelte
@@ -2,6 +2,55 @@
 	import Hero from './hero.svelte';
 
 	export let data;
+
+	$: details = [
+		{
+			label: 'Released',
+			value: data.movie.release_date
+		},
+		{
+			label: 'Runtime',
+			value: `${data.movie.runtime} minutes`
+		},
+		{
+			label: 'Budget',
+			value: `${data.movie.budget / 1e6}M`
+		},
+		{
+			label: 'Revenue',
+			value: `${data.movie.revenue / 1e6}M`
+		},
+		{
+			label: 'Genre',
+			value: data.movie.genres.map((genre) => genre.name).join(', ')
+		}
+	];
 </script>
 
 <Hero movie={data.movie} />
+
+<div class="column grid margin-8 gap-4 md:grid-cols-2">
+	{#if data.trailer}
+		<iframe
+			class="aspect-video w-full"
+			src="https://www.youtube.com/embed/{data.trailer.key}"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+		/>
+	{/if}
+
+	<dl class="grid gap-4 items-baseline">
+		{#each details as details}
+			<dt class="uppercase opacity-80 text-sm">{details.label}</dt>
+			<dd>{details.value}</dd>
+		{/each}
+	</dl>
+</div>
+
+<style>
+	.grid {
+		grid-template-columns: max-content 1fr;
+	}
+</style>
